fbf27ae5b0e63f5c2fe4e39d9c494edfbfa25c11
vladimirfilosof <vfilosof1@gmail.com>
2021-01-04 19:32:32 +0300

    add caesar

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,9 +15,22 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    shift = shift % 26
+    if shift != 0:
+        for ch in plaintext:
+            if 'a' <= ch <= 'z':
+                ch = chr(ord(ch) + shift)
+                if not ('a' <= ch <= 'z'):
+                    ch = chr(ord(ch) - ord('z') + ord('a') - 1)
+            elif 'A' <= ch <= 'Z':
+                ch = chr(ord(ch) + shift)
+                if not ('A' <= ch <= 'Z'):
+                    ch = chr(ord(ch) - ord('Z') + ord('A') - 1)
+            ciphertext += ch
+    else:
+        return plaintext
+            
     return ciphertext
-
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
@@ -33,14 +46,32 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    shift = shift % 26
+    if shift != 0:
+        for ch in ciphertext:
+            if 'a' <= ch <= 'z':
+                ch = chr(ord(ch) - shift)
+                if not ('a' <= ch <= 'z'):
+                    ch = chr(ord(ch) + ord('z') - ord('a') + 1)
+            elif 'A' <= ch <= 'Z':
+                ch = chr(ord(ch) - shift)
+                if not ('A' <= ch <= 'Z'):
+                    ch = chr(ord(ch) + ord('Z') - ord('A') + 1)
+            plaintext += ch
+    else:
+        return ciphertext
+            
     return plaintext
-
 
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
     """
     Brute force breaking a Caesar cipher.
     """
     best_shift = 0
-    # PUT YOUR CODE HERE
+    
+    for i in range(1, 26):
+        if decrypt_caesar(ciphertext, i) in dictionary:
+            best_shift = i;
+
     return best_shift
+
